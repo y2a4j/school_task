f03b8deda4439d6ac92c1c188fb92f8bcc4dd25b
y2a4j <99031165+y2a4j@users.noreply.github.com>
2022-03-15 16:21:23 +0900

    Create PostCodeSearch.java

---
+++ PostCodeSearch.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ZipSearch8 {
+	public static void main(String args[]) {
+		ZipDB3 zd3 = new ZipDB3("郵便番号ファイル");
+
+		Scanner s = new Scanner(System.in);
+
+		System.out.println("郵便番号を入力して下さい");
+		while(s.hasNext()){
+			String line = s.nextLine();
+
+			zd3.searchPrint(line);
+			System.out.println("続けて検索できます。郵便番号を入力して下さい");
+		}
+		s.close();
+	}
+
+}
