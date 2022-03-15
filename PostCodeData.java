ce96475a20bb6e3276f3449e62bc7f408edcd0e3
y2a4j <99031165+y2a4j@users.noreply.github.com>
2022-03-15 16:23:18 +0900

    Create PostCodeData.java

---
+++ PostCodeData.java
@@ -0,0 +1,40 @@
+//郵便番号データ
+public class PostCodeData {
+	String zipcode;
+	String pref;
+	String city;
+	String town;
+
+	public PostCodeData(String zipcode, String pref, String city, String town) {
+		this.zipcode = zipcode;
+		this.pref = pref;
+		this.city = city;
+		this.town = town;
+	}
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	public String getPref() {
+		return pref;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getTown() {
+		return town;
+	}
+
+	@Override
+	public String toString() {
+		return "ZipData [zipcode=" + zipcode + ", pref=" + pref + ", city=" + city + ", town=" + town + "]";
+	}
+
+}
